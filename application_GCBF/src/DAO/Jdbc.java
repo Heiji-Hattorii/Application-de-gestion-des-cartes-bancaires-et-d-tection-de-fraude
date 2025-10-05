56b81729fc51672f71f532c256ff41766f68aefb
Hajar TEBAI <hajarste@gmail.com>
2025-10-05 16:44:54 +0100

    modification de classe de connexion jdbc

--- application_GCBF/src/DAO/Jdbc.java
+++ application_GCBF/src/DAO/Jdbc.java
@@ -3,8 +3,19 @@ import java.sql.Connection;
 import java.sql.DriverManager; 
 import java.sql.SQLException; 
 public class Jdbc { 
-	public static void main(String[] args) throws ClassNotFoundException ,SQLException{
-		Class.forName("com.mysql.cj.jdbc.Driver"); 
-		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3307/banque_db","root","heiji123"); 
-		System.out.println("Heiji ! tu as bien conncte avec ta BDD");
-}}
+    private static final String URL = "jdbc:mysql://localhost:3307/banque_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "heiji123";
+
+    private Jdbc() {} 
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            System.err.println("error jdbc");
+            throw new SQLException(e);
+        }
+    }
+}
